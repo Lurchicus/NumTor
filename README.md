c767b686b13142abe8b560085307587cf2253269
Dan Rhea <dan.rhea@gmail.com>
2025-04-03 02:15:30 -0400

    Implemented f'{foo}' strings. Changed variable names to lower case (though pylint still thinks some of my variables are constants). Added type hints.

--- README.md
+++ README.md
@@ -20,3 +20,4 @@ Number Torture for Python (pick a number between 1 and 100, with a twist).
 - Dan Rhea - Thu Aug 15 02:09:14 2024 -0400 - Added f"" formatting to the info() procedure, and improved docstring. Also added a docstring to the program (pylint seemed to like it)
 - Dan Rhea -  - Changed input to a function that error checks the input first. Fixed a few things pylint didn't like.
 - Dan Rhea - Tue Dec 24 16:51:30 2024 -0400 - Updated the copyright for the first year NumTor (Number Torture for 8-bit Atari BASIC).
+- Dan Rhea - Thu Apr 03 02:09:11 2025 -0400 - Implemented f"{foo}" strings. Changed variable names to lower case (though pylint still thinks some of my variables are constants). Added type hints.
--- numtor.py
+++ numtor.py
@@ -13,12 +13,12 @@ def prompt():
     i_got = -2
     its_good = False
     while not its_good:
-        inp = input(Fore.YELLOW + " Pick a number between 1 and 100: " + Fore.WHITE)
+        inp = input(f"{Fore.YELLOW} Pick a number between 1 and 100 (or 0 to exit): {Fore.WHITE}")
         try:
             i_got = int(inp)
             its_good = True
-        except:
-            print(Back.RED + Fore.WHITE + " Invalid input (not int)!" + Fore.WHITE + Back.RESET)
+        except Exception:
+            print(f"{Back.RED}{Fore.WHITE} Invalid input (not an integer)!{Fore.WHITE}{Back.RESET}")
     return i_got
 
 
@@ -30,93 +30,92 @@ def info(b_flag, target_val, guess_val, threshold_val, in_range_val):
             f"threshold: {threshold_val} in_range: {in_range_val}")
 
 
-#PROMPT = Fore.YELLOW + " Pick a number between 1 and 100: " + Fore.WHITE
-print(Fore.GREEN,"Number Torture by Dan Rhea and Lee Weiss 1991-2024",Fore.WHITE)
-print(Fore.GREEN,"So what's the torture? Well, along with my picking",Fore.WHITE)
-print(Fore.GREEN,"a number between 1 and 100, I'll also pick a value",Fore.WHITE)
-print(Fore.GREEN,"between 2 and 5. If you get near the number without",Fore.WHITE)
-print(Fore.GREEN,"guessing it exactly, I'll change the number!",Fore.WHITE)
-print(Fore.GREEN,"Have fun! (enter 0 to exit)",Fore.WHITE)
+print(f"{Fore.GREEN}Number Torture by Dan Rhea and Lee Weiss 1991-2024")
+print("So what's the torture? Well, along with my picking")
+print("a number between 1 and 100, I'll also pick a value")
+print("between 2 and 5. If you get near the number without")
+print("guessing it exactly, I'll change the number!")
+print(f"Have fun! (enter 0 to exit){Fore.WHITE}")
 
-DEBUG = False                   # debug flag
-BEST = 1000                     # best score so far
-TRIES = 0                       # current number of tries
-IN_RANGE = 101                   # how close the guess is to the target
-GUESS = -1                      # current guess
-THRESHOLD = randint(2,5)        # how close the guess has to be to the target to change it
-TARGET = randint(1, 100)        # the target number
+debug: bool = False             # debug flag
+best: int = 1000                # best score so far
+tries: int = 0                  # current number of tries
+in_range: int = 101             # how close the guess is to the target
+guess: int = -1                 # current guess
+threshold: int = randint(2,5)   # how close the guess has to be to the target to change it
+target: int = randint(1, 100)   # the target number
 
-info(DEBUG, TARGET, GUESS, THRESHOLD, IN_RANGE)
-GUESS = prompt()
-while GUESS != 0:
+info(debug, target, guess, threshold, in_range)
+guess = prompt()
+while guess != 0:
 
-    TRIES = TRIES + 1
+    tries += 1
 
     # Get the value of how close the guess is to the target
-    if GUESS < TARGET:
-        IN_RANGE = abs(TARGET-GUESS)
+    if guess < target:
+        in_range = abs(target-guess)
     else:
-        IN_RANGE = abs(GUESS-TARGET)
+        in_range = abs(guess-target)
 
-    info(DEBUG, TARGET, GUESS, THRESHOLD, IN_RANGE)
+    info(debug, target, guess, threshold, in_range)
 
     # Show tries and best score (if there is one)
-    if GUESS != -1:
-        if BEST < 1000:
-            print(Back.BLUE + Fore.WHITE + " Try: " + str(TRIES) + Back.GREEN + Fore.WHITE +
-                " Your best score is: " + str(BEST) + Fore.WHITE + Back.RESET)
+    if guess != -1:
+        if best < 1000:
+            print(f"{Back.BLUE}{Fore.WHITE} Try: {str(tries)}{Back.GREEN}{Fore.WHITE}"
+                  f" Your best score is: {str(best)}{Fore.WHITE}{Back.RESET}")
         else:
-            print(Back.BLUE + Fore.WHITE + " Try: " + str(TRIES) + Fore.WHITE + Back.RESET)
+            print(f"{Back.BLUE}{Fore.WHITE} Try: {str(tries)}{Fore.WHITE}{Back.RESET}")
 
     # See if we are exiting or not
-    if GUESS != 0:
+    if guess != 0:
 
         # Check for debug toggle
-        if GUESS == -1:
-            if DEBUG:
-                DEBUG = False
+        if guess == -1:
+            if debug:
+                debug = False
             else:
-                DEBUG = True
+                debug = True
             # Decrement the try counter so the toggle doesn't count as a guess
             # and get a new guess
-            TRIES = TRIES - 1
-            info(DEBUG, TARGET, GUESS, THRESHOLD, IN_RANGE)
-            GUESS = prompt()
+            tries -= 1
+            info(debug, target, guess, threshold, in_range)
+            guess = prompt()
             continue
 
         # Logic for a perfect guess
-        if GUESS == TARGET:
-            print(Back.GREEN + Fore.WHITE + " You got it in "+str(TRIES)+"!" +
-                Fore.WHITE + Back.RESET)
-            TARGET = randint(1, 100)
-            THRESHOLD = randint(2,5)
-            GUESS = -2
-            info(DEBUG, TARGET, GUESS, THRESHOLD, IN_RANGE)
+        if guess == target:
+            print(f"{Back.GREEN}{Fore.WHITE} You got it in {str(tries)}!"
+                  f"{Fore.WHITE}{Back.RESET}")
+            target = randint(1, 100)
+            threshold = randint(2,5)
+            guess = -2
+            info(debug, target, guess, threshold, in_range)
 
             # Adjust best score if needed
-            if TRIES < BEST:
-                BEST = TRIES
-                TRIES = 0
+            if tries < best:
+                best = tries
+                tries = 0
 
         else:
             # Logic for too low
-            if GUESS < TARGET:
-                print(Back.RED + Fore.WHITE + " That's too low!" + Fore.WHITE + Back.RESET)
+            if guess < target:
+                print(f"{Back.RED}{Fore.WHITE} That's too low!{Fore.WHITE}{Back.RESET}")
 
             # Logic for too high
-            if GUESS > TARGET:
-                print(Back.RED + Fore.WHITE + " That's too high!" + Fore.WHITE + Back.RESET)
+            if guess > target:
+                print(f"{Back.RED}{Fore.WHITE} That's too high!{Fore.WHITE}{Back.RESET}")
 
             # Logic for a "too close" guess
-            if IN_RANGE <= THRESHOLD:
-                print(Back.RED + Fore.WHITE + " And "+str(GUESS)+ " is too close to "+str(TARGET)+
-                    ", so I changed the number!" + Fore.WHITE + Back.RESET)
-                TARGET = randint(1, 100)
-                THRESHOLD = randint(2,5)
-                info(DEBUG, TARGET, GUESS, THRESHOLD, IN_RANGE)
+            if in_range <= threshold:
+                print(f"{Back.RED}{Fore.WHITE} And {str(guess)} is too close to {str(target)}"
+                      f", so I changed the number!{Fore.WHITE}{Back.RESET}")
+                target = randint(1, 100)
+                threshold = randint(2,5)
+                info(debug, target, guess, threshold, in_range)
 
         # Get the next guess
-        GUESS = prompt()
+        guess = prompt()
 
 # Later
-print(Back.GREEN + Fore.WHITE," Goodbye!",Fore.WHITE + Back.RESET)
+print(f"{Back.GREEN}{Fore.WHITE} Goodbye!{Fore.RESET}{Back.RESET}")
